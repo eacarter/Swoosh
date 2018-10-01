0fe032831dbabd9658d0bae87bc7f29d329666b1
eacarter <eacarter@mtu.edu>
2018-10-01 12:27:56 -0400

    init

---
+++ app/src/main/java/appsolutions/com/swoosh/LeagueActivity.kt
@@ -0,0 +1,13 @@
+package appsolutions.com.swoosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LeagueActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_desired)
+
+    }
+}
--- app/src/main/java/appsolutions/com/swoosh/WelcomeActivity.kt
+++ app/src/main/java/appsolutions/com/swoosh/WelcomeActivity.kt
@@ -1,12 +1,19 @@
 package appsolutions.com.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        start_button.setOnClickListener{
+            val intent = Intent(this, LeagueActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
